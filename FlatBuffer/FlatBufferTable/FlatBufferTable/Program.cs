24c90b122522bf6cba8a8027ba1e2aad2ac741b3
445939112@qq.com <Xjq**gb5059>
2023-10-13 18:43:04 +0800

    [M]FlatBuffer

--- FlatBuffer/FlatBufferTable/FlatBufferTable/Program.cs
+++ FlatBuffer/FlatBufferTable/FlatBufferTable/Program.cs
@@ -5,12 +5,18 @@ namespace GenData
 {
     public class ExportFBDataa
     {
+        static PargmaTypeCheck mPargmaTypeCheck = new PargmaTypeCheck();
+        static private void Main()
+        {
+            Export();
+        }
         static public void Export()
         {
-            var args = Environment.GetCommandLineArgs();
-            FBBinaryExport.TxtRoot = args[1];
-            FBBinaryExport.BinaryRoot = args[2];
-            Console.WriteLine($"TxtRoot:{FBBinaryExport.TxtRoot}\nBinaryRoot:{FBBinaryExport.BinaryRoot}");
+            mPargmaTypeCheck.Init(Environment.GetCommandLineArgs());
+            FBBinaryExport.TxtRoot = mPargmaTypeCheck.GetValue(PargmaType.TxtRoot);
+            FBBinaryExport.BinaryRoot = mPargmaTypeCheck.GetValue(PargmaType.BinaryRoot);
+            FBBinaryExport.csSavePath = mPargmaTypeCheck.GetValue(PargmaType.csSavePath);
+            Console.WriteLine($"TxtRoot:{FBBinaryExport.TxtRoot}\nBinaryRoot:{FBBinaryExport.BinaryRoot} \ncsSavePath:{FBBinaryExport.csSavePath}");
             FBBinaryExport.Export();
         }
     }
